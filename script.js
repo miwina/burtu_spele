dc8352b5b84d5c8630f420ff1f5fcd6c692cbd8b
mariva-lab <80712112+mariva-lab@users.noreply.github.com>
2022-01-08 17:52:06 +0200

    spele

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+let adrese = window.location.hash
+adrese = decodeURI(adrese)
+adrese = adrese.replace('#','')
+adrese = adrese.split(",")
+let vards  = adrese[0]
+document.querySelector('.virsraksts').innerHTML = 'Sveiks '+vards
+
 document.addEventListener('DOMContentLoaded', (event) => {
 
     var dragSrcEl = null;
